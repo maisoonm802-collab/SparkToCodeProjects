f462094deeac769e818ebf71ee58f364c595da6d
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-07 13:40:15 +0300

    complete task 6

--- Task4/Task 4/Program.cs
+++ Task4/Task 4/Program.cs
@@ -59,3 +59,16 @@ Console.WriteLine("\n--- Task 2 ---");
             {
                 Console.WriteLine("Odd");
             }
+            ///Task6\\\\\\\\\\\\
+            Console.WriteLine("\n--- Task 6 ---");
+Console.Write("Enter length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalculateArea(length, width);
+            double perimeter = CalculatePerimeter(length, width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
