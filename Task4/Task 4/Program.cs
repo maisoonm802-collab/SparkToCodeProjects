0fabdaae4b7669d031a2c8c4c06d8cd0f907b5fd
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-07 13:19:55 +0300

    complete task1

---
+++ Task4/Task 4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintWelcome(string name)
+            {
+                Console.WriteLine("Welcome " + name + "!");
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+
+                PrintWelcome(name);
+            }
+        }
+    }
+}
