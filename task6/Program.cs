301d03d4053347a3111c7d925125d8a1fd116e33
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-07 15:42:28 +0300

    COMPLETE TASK 1

---
+++ task6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void FixedGradesArray()
+            {
+                int[] grades = new int[5];
+
+                for (int i = 0; i < grades.Length; i++)
+                {
+                    Console.Write($"Enter grade {i + 1}: ");
+                    grades[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("\nStudent Grades:");
+
+                foreach (int grade in grades)
+                {
+                    Console.WriteLine(grade);
+                }
+            }
+        }
+    }
+}
