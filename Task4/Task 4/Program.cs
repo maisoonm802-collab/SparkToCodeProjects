8f3034914360a85d9877e5c7d2eb5ddd8cdf480f
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-07 13:32:55 +0300

    complete tsk3

--- Task4/Task 4/Program.cs
+++ Task4/Task 4/Program.cs
@@ -25,5 +25,15 @@ Console.WriteLine("\n--- Task 2 ---");
             int result = Square(number);
 
             Console.WriteLine("Square = " + result);
+            ///Task3\\\\\\\\\\\\
+            Console.WriteLine("\n--- Task 3 ---");
+            static double CelsiusToFahrenheit(double celsius)
+            {
+                return (celsius * 9 / 5) + 32;
+            }
 
+            Console.Write("Enter Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = CelsiusToFahrenheit(celsius);
+            Console.WriteLine("Fahrenheit = " + fahrenheit);
 
