b352af04e71fb19c0cf3d95065f625c5d80aa9cd
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-12 22:07:40 +0300

    Complete Parallel Lists

---
+++ project1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        // Parallel Lists
+        static void Main(string[] args)
+        {
+            bool exitApp = false;
+
+            while (!exitApp)
+            {
+                Console.WriteLine("\n===== Welcome to Spark Bank =====");
+                Console.WriteLine("1. Add New Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Show Balance");
+                Console.WriteLine("5. Transfer Amount");
+                Console.WriteLine("6. List All Accounts");
+                Console.WriteLine("7. Close Account");
+                Console.WriteLine("8. Exit");
+
+                Console.Write("Choose an option: ");
+
+                int choice;
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input! Please enter a number.");
+                    continue;
+                }
+
+            }
+        }
+    }
+}
+        
+            
+        
+    
+
+        
+    
+
