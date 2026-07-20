5393e7428aed7088d61bdf07e858ec14f2c7426c
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-20 20:22:14 +0300

    done step2

--- Hotel-Mangment-System/Room.cs
+++ Hotel-Mangment-System/Room.cs
@@ -6,5 +6,29 @@ namespace Hotel_Mangment_System
 {
     internal class Room
     {
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
+
+        public Room(int roomNumber,
+                    string roomType,
+                    double pricePerNight,
+                    bool isAvailable)
+        {
+            this.roomNumber = roomNumber;
+            this.roomType = roomType;
+            this.pricePerNight = pricePerNight;
+            this.isAvailable = isAvailable;
+        }
+
+        public void displayRoom()
+        {
+            Console.WriteLine($"Room Number : {roomNumber}");
+            Console.WriteLine($"Room Type   : {roomType}");
+            Console.WriteLine($"Price       : OMR {pricePerNight:F2}");
+            Console.WriteLine($"Available   : {isAvailable}");
+        }
     }
 }
+
