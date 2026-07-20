129a97c61386bd78608ea3c0441fe1d35d329888
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-20 20:19:01 +0300

    done step 1

---
+++ Hotel-Mangment-System/Guest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel_Mangment_System
+{
+    internal class Guest
+    {
+        public string guestId;
+        public string guestName;
+        public string roomNumber;
+        public string checkInDate;
+        public int totalNights;
+
+        public Guest(string guestId,
+                     string guestName,
+                     string roomNumber,
+                     string checkInDate,
+                     int totalNights)
+        {
+            this.guestId = guestId;
+            this.guestName = guestName;
+            this.roomNumber = roomNumber;
+            this.checkInDate = checkInDate;
+            this.totalNights = totalNights;
+        }
+    }
+}
+
---
+++ Hotel-Mangment-System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Mangment_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
