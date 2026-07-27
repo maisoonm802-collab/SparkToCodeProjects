2ede1ed1aa6b4a33d6ba29ddb60deee4e5abae1f
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-28 02:55:01 +0300

    Add user.cs

---
+++ ECommerceApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ECommerceApp
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public List<Order> Orders { get; set; } = new();
+    }
+}
