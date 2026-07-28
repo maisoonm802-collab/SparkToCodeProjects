7a9f46aa8a215bf530b97cdb2cb7a615dd4d4c24
maisoonm802-collab <maisoonm802@gmail.com>
2026-07-28 03:02:25 +0300

    add orders.cs

---
+++ ECommerceApp/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceApp
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public List<OrderProduct> OrderProducts { get; set; } = new();
+
+        public Review? Review { get; set; }
+    }
---
+++ ECommerceApp/Models/Prodcut.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ECommerceApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public List<OrderProduct> OrderProducts { get; set; } = new();
+    }
+}
+
